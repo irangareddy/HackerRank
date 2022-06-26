94e30b506e667c2b5a06424f7e4d91149c8780ef
irangareddy <itsmerr17@gmail.com>
2022-06-26 21:49:38 +0530

    subarray work around

--- Warmup/subarray_division.py
+++ Warmup/subarray_division.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
 def birthday(s, d, m):
-    values = [x for x in range(len(s)) if s[x] + s]
-            
-birthday(s=[2,2,1,3,2],d=4,m=2)
+    print(s[0:2])
+    result = [x for i in range(len(s)) if s[i]==2]
+    print(result)
+    return sum([1 for i in range(len(s)-m+1) if sum(s[i:i+m])==d])
+
+    
+
+result = birthday(s=[1,2,3,4,5],d=3,m=3)
+print(result)
